0853f6bc32be819298a9e5ce99ddb33153a0b06e
Marcos Piemontez <marcos.piemontez1@hotmail.com>
2021-11-23 15:45:14 -0300

    🔧 CONFIG: Configuração da politica "cors"

--- src/app.js
+++ src/app.js
@@ -1,21 +1,24 @@
-// arquivo de execução do banco de dados!
-
 const express = require('express')
 const routers = require('./controllers/index')
 const { configSequelize } = require('./models/index')
+const cors = require('cors')
 
 const app = express()
+
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use('/', routers)
 
-
-// { force:true }
-configSequelize.sync({ force:true }).then(() => {
-    console.log('Database Connected')
+// { force: true }
+configSequelize.sync().then(() => {
+    console.log('Database Connected!')
 })
 
-// porta
 app.listen(3000, () => {
     console.log('Application online!')
 })
