1a9f5dcd0ad859a34b53a19119e2366683c741b1
Marcos Piemontez <marcos.piemontez1@hotmail.com>
2021-11-23 14:48:58 -0300

    🔨 REFACTOR: Correção da rota de cadastro para acesso publico.

--- src/controllers/authentication.js
+++ src/controllers/authentication.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const { Users } = require('../models/index')    // usando o user que foi centralizado no index
 const AuthenticatedService = require('../services/authentication')   //classe é sempre maiúsculo
-const { check, validationResult } = require('express-validator')
+const { body, check, validationResult } = require('express-validator')
+const UserService = require('../services/users')
 const jwt = require('jsonwebtoken')
 const keyToken = require('../config/keyToken.json')
 const mailer = require('../util/mailer')
 
 const router = express.Router()
 const authenticatedService = new AuthenticatedService(Users)
+
+const userService = new UserService(Users)
 
 function generateToken(id = {}){
     return jwt.sign(
@@ -16,6 +19,47 @@ function generateToken(id = {}){
         {expiresIn: '12h'}
     )
 }
+
+router.post('/registration',
+    body('name').not().isEmpty().withMessage('The data sent connot be empty or null').trim().escape(),
+    check('cpf').not().isEmpty().isNumeric().withMessage('The data sent must be valid numbers'),
+    check('birthDate').not().isEmpty().isISO8601().toDate().withMessage('The date entered is not within the established pattern'),
+    check('password').not().isEmpty().isLength({ min: 6 }).withMessage('The password entered must have more than 6 characters'),
+    check('email').not().isEmpty().isEmail().withMessage('The e-mail entered is not a valid address'),
+
+    async (req, res) => {
+
+        const errors = validationResult(req)
+
+        if(!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            })
+        }
+
+        const dataUser = {
+            name: req.body.name,
+            cpf: req.body.cpf,
+            birthDate: req.body.birthDate,
+            password: req.body.password,
+            email: req.body.email,
+            typeAccessId: req.body.typeAccessId
+        }
+
+        try {
+            await userService.add(dataUser)
+            res.status(201).send({
+                status: 201,
+                message: 'User successfully added!'
+            })
+        } catch (error) {
+            res.status(400).send({
+                status:400,
+                message: error.message
+            })
+        }
+    }
+)
 
 router.post('/loginUser',
     check('password').not().isEmpty().isLength({ min: 6 }).withMessage('The password ented must have more than 6 characters'),
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -33,47 +33,6 @@ router.get('/list', async (req, res) => {
     res.status(200).json(listUsers)
 })
 
-router.post('/',
-    body('name').not().isEmpty().withMessage('The data sent connot be empty or null').trim().escape(),
-    check('cpf').not().isEmpty().isNumeric().withMessage('The data sent must be valid numbers'),
-    check('birthDate').not().isEmpty().isISO8601().toDate().withMessage('The date entered is not within the established pattern'),
-    check('password').not().isEmpty().isLength({ min: 6 }).withMessage('The password entered must have more than 6 characters'),
-    check('email').not().isEmpty().isEmail().withMessage('The e-mail entered is not a valid address'),
-
-    async (req, res) => {
-
-        const errors = validationResult(req)
-
-        if(!errors.isEmpty()) {
-            return res.status(400).json({
-                errors: errors.array()
-            })
-        }
-
-        const dataUser = {
-            name: req.body.name,
-            cpf: req.body.cpf,
-            birthDate: req.body.birthDate,
-            password: req.body.password,
-            email: req.body.email,
-            typeAccessId: req.body.typeAccessId
-        }
-
-        try {
-            await userService.add(dataUser)
-            res.status(201).send({
-                status: 201,
-                message: 'User successfully added!'
-            })
-        } catch (error) {
-            res.status(400).send({
-                status:400,
-                message: error.message
-            })
-        }
-    }
-)
-
 router.put('/:id',
     body('name').not().isEmpty().withMessage('The data sent cannot be empty or null').trim().escape(),
     check('cpf').not().isEmpty().isNumeric().withMessage('The data sent must be valid numbers'),
